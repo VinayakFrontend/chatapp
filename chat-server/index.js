e912853d2aadc4de1b7819f3d2b28d3f53db6563
VinayakFrontend <guptavinayak1614@gmail.com>
2025-04-24 20:14:17 +0530

    Update index.js

--- chat-server/index.js
+++ chat-server/index.js
@@ -17,36 +17,30 @@ const io = new Server(server, {
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
 
-  // When the client sets the user name
   socket.on('set_user_name', (name) => {
-    socket.userName = name; // Store the user's name
+    socket.userName = name;
   });
 
-  // Handle receiving messages
   socket.on('send_message', (data) => {
-    io.emit('receive_message', data); // Broadcast message to all users
+    io.emit('receive_message', data);
   });
 
-  // Handle typing notifications
   socket.on('user_typing', (userName) => {
-    socket.broadcast.emit('user_typing', userName); // Notify others that a user is typing
+    socket.broadcast.emit('user_typing', userName);
   });
 
-  // Handle user leaving
   socket.on('disconnect', () => {
     if (socket.userName) {
-      io.emit('user_left', socket.userName); // Broadcast to all that the user has left
+      io.emit('user_left', socket.userName);
       console.log(`${socket.userName} disconnected`);
     }
   });
 
-  // When a user explicitly leaves the chat
   socket.on('user_left', (userName) => {
-    io.emit('user_left', userName); // Broadcast the user leaving
+    io.emit('user_left', userName);
     console.log(`${userName} left the chat`);
   });
 });
+
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-});
